003354defdeb6f18cba60320b7a486a988411f07
CotzaDev <andrea@cotza.me>
2016-05-11 14:52:41 +0200

    Test

--- docker/image-auditor/auditor.js
+++ docker/image-auditor/auditor.js
@@ -66,7 +66,8 @@ server.on('listening', function() {
 });
 
 server.on('connection', function(socket) {
-    socket.end(makePayload());
+    socket.write(makePayload())
+    socket.end();
     console.log("Payload sent to %s:%d", socket.remoteAddress, socket.remotePort);
 });
 
